a8ecd5bf49fbbb85c25163d6533f0a440c751a40
Pedro Henrique <pedrohenriquesb4002@gmail.com>
2026-05-28 14:13:12 -0300

    AGORA VAII

--- api/auth.js
+++ api/auth.js
@@ -65,7 +65,7 @@ module.exports = async (req, res) => {
   } finally {
     client.release();
   }
-  export default async function handler(req, res){
+  module.exports = async function handler(req, res){
 
   if(req.method !== 'POST'){
     return res.status(405).json({
